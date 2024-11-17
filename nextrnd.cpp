83891fcb7ce56be43cc5a27115f07972a759ae8b
KRITHIN913 <ngkgsid@gmail.com>
2024-11-18 00:55:15 +0530

    debugged the for loop

---
+++ nextrnd.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int n,k;
+    cin>>n;
+    cin>>k;
+    vector <int> scores(n);
+    
+    for (auto& score:scores )
+    {
+        cin>>score;
+    }
+    
+
+    int t = scores[k-1];
+    int count = 0;
+
+    for(auto& score:scores)
+    {
+        if( score>=t && score >0)
+            count++;
+    }
+
+    cout <<count<<endl;
+
+
+    return 0;
+}
